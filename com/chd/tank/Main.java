0efd891679e47dc2938240064ae928162fe7c162
Yhton <875019750@qq.com>
2019-06-01 21:04:58 +0800

    init

--- com/chd/tank/Main.java
+++ com/chd/tank/Main.java
@@ -4,7 +4,7 @@ package com.chd.tank;
 public class Main {
     /**
      * 初识 Frame
-     * test
+     *
      */
     public static void main(String[] args) {
         TankFrame tankFrame = new TankFrame();
